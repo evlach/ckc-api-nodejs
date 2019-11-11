8913d3954af12718624aafdc6203f56fa1795fa5
Evangelos Vlachogiannis <evangelos.vlachogiannis@gmail.com>
2019-11-11 10:20:28 +0200

    add initial test for connect

--- test/client.test.js
+++ test/client.test.js
@@ -1,16 +1,13 @@
 const assert = require("assert");
 const CKC = require("../index");
 
-describe("#client", () =>
-{
-	describe("#connect", () =>
-	{
-
-		it("should authenticate, get auth data and cache it for further requests", async () =>
-		{
-			const client = await CKC.Client.connect();
-			assert.isDefined(client.authData);
-		});
-	});
-
+describe("#client", () => {
+  describe("#connect", () => {
+    it("should authenticate, get auth data and cache it for further requests", async () => {
+      const client = await CKC.Client.connect();
+      assert(client.authData !== null);
+      assert(typeof client.authData.access_token === "string");
+      assert(client.authData.access_token.length > 0);
+    });
+  });
 });
